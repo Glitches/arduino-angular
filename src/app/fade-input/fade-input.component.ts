b94576095d4ceea0d74a97782668cb159b8a6b6a
Andrea Ceccarelli <gltch2003@gmail.com>
2018-03-17 17:23:06 +0100

    Feature: add fade function

--- src/app/fade-input/fade-input.component.ts
+++ src/app/fade-input/fade-input.component.ts
@@ -1,15 +1,36 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, EventEmitter, Output, ViewChild, ElementRef } from '@angular/core';
+import { Observable } from "rxjs";
+import { LedServiceService } from '../led-service.service';
 
+let fadeTime = '';
 @Component({
-  selector: 'app-fade-input',
-  templateUrl: './fade-input.component.html',
-  styleUrls: ['./fade-input.component.css']
+  selector: "app-fade-input",
+  templateUrl: "./fade-input.component.html",
+  styleUrls: ["./fade-input.component.css"]
 })
 export class FadeInputComponent implements OnInit {
+  @Output() public onSearch: EventEmitter<String> = new EventEmitter();
 
-  constructor() { }
+  @ViewChild("searchInput") private searchInput: ElementRef;
 
-  ngOnInit() {
+  constructor(private ledService: LedServiceService) {}
+
+ ngOnInit() {
+    Observable.fromEvent(
+      this.searchInput.nativeElement, 'keyup'
+    )
+    .debounceTime(500)
+    .subscribe((event:any) => this.searchQueryChanged(event.target.value);
   }
 
+  searchQueryChanged(query: string) {
+    console.log(query)
+    this.onSearch.emit(query);
+    fadeTime = query;
+  }
+
+    public getFade(event) {
+      this.ledService.strobeLed(`/fade?time=${fadeTime}`);
+    } 
+  
 }
